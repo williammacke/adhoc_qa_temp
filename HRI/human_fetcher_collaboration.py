f8e9e770f9499bdb0b6faa3004f8ff3e5e3d2103
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-06-18 17:34:41 -0500

    Flip screen (match original render)

--- HRI/human_fetcher_collaboration.py
+++ HRI/human_fetcher_collaboration.py
@@ -58,23 +58,23 @@ class _GetchWindows:
 
 arrived = False
 
-# def get_worker_action():
-#     global arrived
-#     #fill in code for human input here
-#     while (True):
-#         if (arrived):
-#             return Input["J"].value
-#         getch = _Getch()
-#         val = getch().upper()
+def get_worker_action():
+    global arrived
+    #fill in code for human input here
+    while (True):
+        if (arrived):
+            return Input["J"].value
+        getch = _Getch()
+        val = getch().upper()
   
-#         if (val in Input.__members__):
-#             if(val == "J"):
-#                 arrived = True
-#             return Input[val].value
-#         elif (val == "Z"):
-#             exit()
-#         else: 
-#             print ("Not a valid input, please try again.")
+        if (val in Input.__members__):
+            if(val == "J"):
+                arrived = True
+            return Input[val].value
+        elif (val == "Z"):
+            exit()
+        else: 
+            print ("Not a valid input, please try again.")
 
 if __name__ == '__main__':
     #Fetcher start position
@@ -89,7 +89,7 @@ if __name__ == '__main__':
     #List of tool positions, in this example they are all located in the same spot
     # tool_pos = [np.array([2,3]) for _ in range(3)]
     tool_pos = [np.array([3,5]) for _ in range(3)]
-    goal_stn = 2
+    goal_stn = 0
     env = ToolFetchingEnvironment(fetcher_pos, worker_pos, stn_pos, tool_pos, goal_stn)
     #grab observation
     obs = env.reset()
@@ -97,10 +97,10 @@ if __name__ == '__main__':
     fetcher = FetcherQueryPolicy()
     worker = RandomWorkerPolicy()
     #Print goal/positions for human to determine where to go
-    print("The fetcher is at", fetcher_pos)
-    print("You (the worker) is at", worker_pos)
-    print("The fetcher's tool box is at", tool_pos[0])
-    print("Your goal position is at", stn_pos[goal_stn])
+    # print("The fetcher is at", fetcher_pos)
+    # print("You (the worker) is at", worker_pos)
+    # print("The fetcher's tool box is at", tool_pos[0])
+    print("Your goal station is station", goal_stn)
     print("Press\n W - up\n A - left\n S - down\n D - right\n J - done (press when arrived at station)\n Z - exit")
 
     gui = GUI(10, 6, stn_pos, tool_pos, worker_pos, fetcher_pos)
@@ -108,13 +108,18 @@ if __name__ == '__main__':
     #run until done
     while not done[0]:
         # only needed for rendering
-        # env.render()
+        env.render()
+        
 
-        returnVal = gui.on_execute()
+        sleep(0.05)
+
+        fetcher_move = fetcher(obs[1])
+        returnVal = gui.on_execute(fetcher_move[0]) # returns with input value
         if(returnVal == Input.Exit):
             break
         
-        sleep(0.05)
-        # obs, reward, done, _ = env.step([returnVal.value, fetcher(obs[1])])
-    # env.close()
+        obs, reward, done, _ = env.step([returnVal.value, fetcher_move])
+        # obs, reward, done, _ = env.step([get_worker_action(), fetcher(obs[1])])
+
+    env.close()
     gui.on_cleanup()
--- HRI/pygame_gui.py
+++ HRI/pygame_gui.py
@@ -27,11 +27,18 @@ class GUI:
         self.screen = None
         self.clock = None
         self.size = self.width, self.height = 640, 400
+        self.num_rows = num_rows
 
         self.user_x = worker_pos[0]
         self.user_y = worker_pos[1]
         self.prev_user_x = worker_pos[0]
         self.prev_user_y = worker_pos[1]
+
+        self.robot_x = fetcher_pos[0]
+        self.robot_y = fetcher_pos[1]
+        self.prev_robot_x = fetcher_pos[0]
+        self.prev_robot_y = fetcher_pos[1]
+        self.robot_stay = False
 
         self.box_width = self.width / num_cols
         self.box_height = self.height / num_rows
@@ -49,7 +56,7 @@ class GUI:
     # Rectangular station
     def render_station(self, color, stn):
         rect = [self.box_width * stn[0] + self.x_margin, 
-                self.box_height * stn[1] + self.y_margin, 
+                self.box_height * (self.num_rows - 1 - stn[1]) + self.y_margin, 
                 self.box_width - (self.x_margin * 2), 
                 self.box_height - (self.y_margin * 2)]
 
@@ -62,13 +69,13 @@ class GUI:
     # Circular agent
     def render_agent(self, circle_x, circle_y, color):
         gui_x = circle_x * self.box_width + (self.box_width / 2)
-        gui_y = circle_y * self.box_height + (self.box_height / 2)
+        gui_y = (self.num_rows - 1 - circle_y) * self.box_height + (self.box_height / 2)
         pygame.draw.circle(self.screen, color, (int(gui_x), int(gui_y)), int(self.radius))
 
     # Text within station or agent
     def render_text(self, textString, box_x, box_y):
         text_x = box_x * self.box_width + self.x_margin * 3
-        text_y = box_y * self.box_height + self.y_margin * 3
+        text_y = (self.num_rows - 1 - box_y) * self.box_height + self.y_margin * 3
 
         text = self.font.render(textString, True, WHITE)
         self.screen.blit(text,
@@ -118,7 +125,7 @@ class GUI:
 
         # Agents
         # Worker
-        self.render_agent(worker_pos[0], worker_pos[1], GREEN)
+        self.render_agent(worker_pos[0], worker_pos[1], BLACK)
         self.render_text("W",  worker_pos[0], worker_pos[1])
         
         # Fetcher
@@ -157,9 +164,18 @@ class GUI:
     # Render drawing
     def on_render(self):
         if(self.running):
-            self.render_agent(self.user_x, self.user_y, GREEN)
-            self.render_text("W", self.user_x, self.user_y)
-            self.render_agent(self.prev_user_x, self.prev_user_y, WHITE) # Remove old agent
+            #User
+            # self.render_agent(self.user_x, self.user_y, GREEN)
+            # self.render_text("W", self.user_x, self.user_y)
+            # self.render_agent(self.prev_user_x, self.prev_user_y, WHITE) # Remove old user agent
+
+            # #Robot
+            # if self.robot_stay:
+            #     self.robot_stay = False
+            # else:
+            #     self.render_agent(self.robot_x, self.robot_y, GREEN)
+            #     self.render_text("F", self.robot_x, self.robot_y)
+            #     self.render_agent(self.prev_robot_x, self.prev_robot_y, WHITE) # Remove old robot agent
 
             # pygame.draw.rect(self.screen, RED, [self.user_x, self.user_y, 20, 20])
             # pygame.draw.rect(self.screen, WHITE, [self.prev_user_x, self.prev_user_y, 20, 20])
@@ -168,7 +184,24 @@ class GUI:
     def on_cleanup(self):
         pygame.quit()
     
-    def on_execute(self):
+    def _move_worker(self, move):
+        self.prev_robot_x = self.robot_x
+        self.prev_robot_y = self.robot_y
+
+        if move == 0:
+            self.robot_x += 1
+        elif move == 1:
+            self.robot_x -= 1
+        elif move == 2:
+            self.robot_y -= 1
+        elif move == 3:
+            self.robot_y += 1
+        else: #query or pickup
+            self.robot_stay = True
+            
+
+    def on_execute(self, other_agent_move):
+        self._move_worker(other_agent_move)
         action = None
         while self.running:
             for event in pygame.event.get():
