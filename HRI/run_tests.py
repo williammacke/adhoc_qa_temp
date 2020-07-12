05a22840a793725bcf4315a4104438e53c42d031
Jennifer Suriadinata <jsuriadinata@utexas.edu>
2020-07-11 20:34:24 -0500

    Add all experiments

--- HRI/run_tests.py
+++ HRI/run_tests.py
@@ -1,5 +1,21 @@
 import os
+import random
+from zipfile import ZipFile
 
+# Enable environment
+os.system("env/Scripts/activate")
 
-os.system("env\\Scripts\\activate")
-os.system("py -3.6 HRI\\human_fetcher_collaboration.py")
+# Randomize experiments
+exp_num = [1, 2, 3, 4]
+random.shuffle(exp_num)
+for x in exp_num:
+    # Run test
+    os.system("py -3.6 HRI/human_experiment.py -i HRI/experiment"+ str(x) + ".txt -o output" + str(x) + ".txt")
+
+# Zip up results
+zipResults = ZipFile('results.zip', 'w')
+
+for x in range(1,5):
+    zipResults.write('output' + str(x) + '.txt') 
+    os.remove('output' + str(x) + '.txt')
+zipResults.close()
